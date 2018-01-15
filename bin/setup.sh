bc8168793bb6926d6c111ab17bf82a0df855ad2a
nurik23 <nurdoolot.tagaev>
2018-01-15 23:11:04 +0600

    #TODO ready

--- bin/setup.sh
+++ bin/setup.sh
@@ -36,10 +36,83 @@ if [ ! -d "$environments" ]; then
 fi
 
 # TODO: check for libvirt
+Libvirt(){
+lib=`virsh --version`
+if [ "$?" != "0" ]; then
+   echo " 'virsh' is currently not installed "
+   echo -n "Enter 'Yes' if you wont to install libvert-bin : "
+   read x
+   if [ $x == "Yes" ];then
+   sudo apt install qemu-kvm libvirt-bin
+   sudo apt-get -f install
+  # sudo adduser $USER libvirtd
+   echo "Libvirt version: $lib"| tail -n1
+   fi 
+else [ "$?" == "0" ]            
+   echo "Libvirt version: $lib"| tail -n1
+fi
+}
+
+Libvirt
+
 # TODO: check for vagrant-libvirt
+VagrantLibvirt(){
+sudo vagrant plugin list
+
+echo -n "Enter 'Yes' if you want to install vagrant-libvirt : "
+   read x
+   if [ $x == "Yes" ];then
+   wget http://production.cf.rubygems.org/rubygems/rubygems-update-2.0.3.gem
+   gem install rubygems-update-2.0.3.gem
+   sudo update_rubygems
+   sudo apt-get build-dep vagrant ruby-libvirt
+   sudo apt-get install -y qemu libvirt-bin ebtables dnsmasq
+   sudo apt-get install -y libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev
+   sudo vagrant plugin install vagrant-libvirt 
+   fi 
+}
+
+VagrantLibvirt
+
 # TODO: check for vagrant
+Vagrant(){
+var=`vagrant --version`
+if [ "$?" != "0" ]; then
+   echo " 'vagrant' is currently not installed "
+   echo -n "Enter 'Yes' if you wont to install vagrant :  "
+   read x
+   if [ $x == "Yes" ];then
+   wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb	
+   sudo dpkg -i vagrant_2.0.1_x86_64.deb
+   echo "$var"| tail -n1
+   fi 
+else [ "$?" == "0" ]            
+   echo "$var"| tail -n1
+fi
+}
+
+Vagrant
+
 # TODO: check for proper networking setup
+Ping(){
+ping -c1 -w7 8.8.8.8 &>/dev/null
+if [ "$?" != "0" ]; then
+   echo "Ping failed"
+else [ "$?" == "0" ]            
+   echo "Ping successful"
+fi
+}
+
+Ping
+
 # TODO: check for bridge device on local machine
+Bridge(){
+echo "bridge device on local machine : "
+brctl show ##| awk 'FNR == 2 {print $1}'
+}
+
+Bridge
+
 
 echo "Cleaning up past runs ..."
 echo "    => " $(cleanup.sh)
